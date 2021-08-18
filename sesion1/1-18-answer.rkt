ab8eeb3a5006d4c18680ff16525f33200b1d96ad
HLhhh <1726893505@qq.com>
2021-08-18 22:34:06 +0800

    使用+、double、halve实现一个lisp的乘法表达式

---
+++ sesion1/1-18-answer.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+(define (* a b)
+  (define (*-iter a b c)
+    (cond ((= b 0) 0)
+          ((= b 1) (+ a c))
+          ((even? b) (*-iter (double a) (halve b) c))
+          (else (*-iter a (- b 1) (+ c a)))
+    ))
+  (*-iter a b 0)
+  )
+
+(define (even? n)
+  (= (remainder n 2) 0))
+(define (double x)
+  (+ x x))
+(define (halve x)
+  (/ x 2))
